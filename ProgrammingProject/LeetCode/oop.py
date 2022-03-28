90d3cb69ef72b2f05ec90820feb53570fe8a048f
Abdul Hadi <37131427+hadizainurin@users.noreply.github.com>
2022-03-28 19:28:44 +0800

    Delete ProgrammingProject directory

--- ProgrammingProject/LeetCode/oop.py
+++
@@ -1,8 +0,0 @@
-#Object is an instntiation of a class. When Class is definied, only the description for the object is defined.
-#Therefore, no mermoy or storage is allocated.
-#In Python, everything is an object, while in C++ you have class then you need to create
-# the object, then public/private(default)/protected access specifier and attribute (data type) 
-#Let create an object of parrot class
-#obj = Parrot()
-
-#class Parrot:
